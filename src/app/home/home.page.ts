cd1737ef2c669231d8a3a31571416bfed4fb357b
sergiocardozo <scardozo.sc@gmail.com>
2023-06-16 23:52:55 -0300

    fixed: home Cliente(test)

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,14 +9,17 @@ import { FirestoreService } from '../services/firestore.service';
 import { BarraComponent } from 'src/app/barra/barra.component';
 import { SpinnerComponent } from '../spinner/spinner.component';
 import { QrscannerService } from '../services/qrscanner.service';
-import { ScannerComponent } from '../component/scanner/scanner.component';
+import Swal from 'sweetalert2';
+import { ListaEspera } from '../shared/listaEspera.interface';
+import { PushnotificationService } from '../services/pushnotification.service';
+import { ListaEsperaService } from '../services/lista-espera.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, BarraComponent, SpinnerComponent, ScannerComponent]
+  imports: [IonicModule, CommonModule, FormsModule, BarraComponent, SpinnerComponent]
 })
 export class HomePage implements OnInit, OnDestroy {
   user: any = null;
@@ -25,6 +28,7 @@ export class HomePage implements OnInit, OnDestroy {
   waitlist = false;
   grid = false;
 
+  hasWait: any = null;
   scanActive = false;
   currentScan:string[];
 
@@ -32,7 +36,9 @@ export class HomePage implements OnInit, OnDestroy {
     private authService: AuthService,
     private router: Router,
     private firestoreService: FirestoreService,
-    private qrSrv: QrscannerService
+    private qrSrv: QrscannerService,
+    private pnSrv: PushnotificationService,
+    private listSrv: ListaEsperaService
   ) { }
 
   ngOnInit() {
@@ -53,19 +59,62 @@ export class HomePage implements OnInit, OnDestroy {
     this.user = null;
     localStorage.removeItem('user');
   }
-  /* escanearQR() {
+  private checkWait() {
+    const a = this.listSrv.getLastByUser(this.user.correo)
+      .subscribe(data => {
+        this.hasWait = data;
+      });
+  }
+
+  private addToWaitList() {
+    try {
+      const m = this.createModelWait();
+      this.listSrv.createOne(m);
+
+    }
+    catch (error) {
+    }
+  }
+  private createModelWait() {
+    let m: ListaEspera = {
+      id: '',
+      estado: 'PENDIENTE',
+      correo: this.user.correo,
+      date_created: new Date().getTime(),
+      user_uid: this.user.uid
+    }
+
+    return m;
+  }
+  async toast(title: string, icono: any, text?: string) {
+    await Swal.fire({
+      title: title,
+      text: text,
+      icon: icono,
+      timer: 3000,
+      toast: true,
+      position: 'top',
+      grow: 'row',
+      showConfirmButton: false,
+      timerProgressBar: true
+    })
+  }
+  escanearQR() {
     this.scanActive = true;
     this.qrSrv.startScan().then((result) => {
       console.log(result);
-      if(result === 'ENTRADALOCAL') {
-        this.scanActive = false;
-        this.waitlist = true;
-        this.grid = false
+      if (result === 'ENTRADALOCAL') {
+        if(!this.hasWait) {
+          this.scanActive = false;
+          this.waitlist = true;
+          this.grid = false
+          this.toast('Ingreso al local', 'Aguarde mientras se le asigna una mesa', 'success');
+          this.addToWaitList();
+          this.pnSrv.enviarNotificacionUsuarios('METRE', 'Ingreso al local', 'Un cliente solicitó la entrada al local', true);
+        }
       }
-      
-
     })
-  } */
+  }
   stopScan() {
     this.scanActive = false;
     this.qrSrv.stopScanner();
